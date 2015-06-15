c9e7e91d512f3f02066fac528fbd0f2640b1e0b1
QDevor <QDevor@163.com>
2015-06-15 15:46:33 +0800

    dirty

---
+++ util/http.go
@@ -0,0 +1,21 @@
+// Copyright (c) 2015 QDevor. All Rights Reserved.
+// Use of this source code is governed by a MIT-style license that can
+// be found in the LICENSE file.
+
+package util
+
+import (
+  `io/ioutil`
+	`net/http`
+	`strings`
+)
+
+func ResponseToString(response *http.Response) (string, error) {
+	content, err := ResponseToByteArray(response)
+	return string(content), err
+}
+
+func ResponseToByteArray(response *http.Response) ([]byte, error) {
+	body := response.Body
+	return ioutil.ReadAll(body)
+}
